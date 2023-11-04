4bd6db980fa7fc14d1e4f27d12d901c529ba641a
Finley <trinity.tringl@gmail.com>
2023-11-04 19:31:19 +1000

    lots of changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -18,14 +18,47 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
     else:
-        pass
+        tasks = Todo.query.order_by(Todo.data_created).all()
+        return render_template('index.html', tasks=tasks)
 
-    return render_template('index.html')
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
 
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True)
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_update.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task_update)
 
 # venv\Scripts\activate
